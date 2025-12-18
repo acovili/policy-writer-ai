b251f5fc8970bccdcc59c76b36d1286379be577f
acovili <alexander.covili@missionary.org>
2025-12-17 21:16:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-
-app.use(express.json());
-app.use(cors());
 
 app.get("/", (req, res) => res.send("Server is live"));
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
